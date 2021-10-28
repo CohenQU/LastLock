faeeac52bb93320ed48b06e7816506cda91c28b5
yuxiao98@uw.edu <yuxiao1155@gmail.com>
2021-10-27 20:37:15 -0500

    init

---
+++ lastlock/urls.py
@@ -0,0 +1,13 @@
+import debug_toolbar
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('admin/', admin.site.urls),
+
+    path('account/', include('account.urls')),
+    path('ping/', include('ping.urls')),
+    path('main/', include('main.urls')),
+    path('blog/', include('blog.urls'))
+]
---
+++ ping/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Account(models.Model):
+    ## Django create unique ID automatically if no specification
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
+# class Lock(models.Model):
+#     account = models.ForeignKey(Account, )
